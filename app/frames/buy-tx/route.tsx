4541b7047226e4a9176dcd75aa05b45034df2539
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-03-30 02:36:07 +0200

    fix: handle errors

--- app/frames/buy-tx/route.tsx
+++ app/frames/buy-tx/route.tsx
@@ -8,67 +8,72 @@ import { TransactionTargetResponse, getFrameMessage } from "frames.js";
 import { DUMMY_TX_ADDRESS } from "../../const";
 
 export const POST = async function (req: NextRequest) {
-  const json = await req.json();
-  const { connectedAddress } = await getFrameMessage(json, {
-    fetchHubContext: false,
-  });
+  try {
+    const json = await req.json();
+    const { connectedAddress } = await getFrameMessage(json, {
+      fetchHubContext: false,
+    });
 
-  if (!connectedAddress) {
-    throw new Error("Message not found");
+    if (!connectedAddress) {
+      throw new Error("Message not found");
+    }
+
+    const key = req.nextUrl.searchParams.get("key");
+
+    if (!key) {
+      throw new Error("Key not found");
+    }
+
+    const value = await kv.get<{
+      quote: SwapRoute;
+      params: Parameters<typeof getSwapTransaction>[0];
+    }>(key);
+
+    if (!value) {
+      throw new Error("Quote not found");
+    }
+
+    const { quote, params } = value;
+
+    if (!quote.methodParameters) {
+      throw new Error("Method parameters not found");
+    }
+
+    // Replace dummy address in calldata
+    const tx = {
+      ...quote.methodParameters,
+      data: quote.methodParameters.calldata.replace(
+        DUMMY_TX_ADDRESS.slice(2),
+        connectedAddress.slice(2)
+      ),
+    };
+
+    const { steps, fundsChainId } = await createRelayCallAuto({
+      call: {
+        destinationChainId: value.params.chainId,
+        txs: [tx],
+      },
+      connectedAddress: connectedAddress as `0x${string}`,
+    });
+
+    kv.set(key, JSON.stringify({ ...value, steps }));
+
+    const relayTxData = steps[0].items?.[0].data;
+
+    const txResponse: TransactionTargetResponse = {
+      chainId: `eip155:${fundsChainId}`,
+      method: "eth_sendTransaction",
+      params: {
+        abi: [],
+        to: relayTxData.to,
+        value: relayTxData.value,
+        data: relayTxData.data,
+      },
+    };
+
+    return Response.json(txResponse);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: "Something went wrong" }, { status: 500 });
   }
-
-  const key = req.nextUrl.searchParams.get("key");
-
-  if (!key) {
-    throw new Error("Key not found");
-  }
-
-  const value = await kv.get<{
-    quote: SwapRoute;
-    params: Parameters<typeof getSwapTransaction>[0];
-  }>(key);
-
-  if (!value) {
-    throw new Error("Quote not found");
-  }
-
-  const { quote, params } = value;
-
-  if (!quote.methodParameters) {
-    throw new Error("Method parameters not found");
-  }
-
-  // Replace dummy address in calldata
-  const tx = {
-    ...quote.methodParameters,
-    data: quote.methodParameters.calldata.replace(
-      DUMMY_TX_ADDRESS.slice(2),
-      connectedAddress.slice(2)
-    ),
-  };
-
-  const { steps, fundsChainId } = await createRelayCallAuto({
-    call: {
-      destinationChainId: value.params.chainId,
-      txs: [tx],
-    },
-    connectedAddress: connectedAddress as `0x${string}`,
-  });
-
-  kv.set(key, JSON.stringify({ ...value, steps }));
-
-  const relayTxData = steps[0].items?.[0].data;
-
-  const txResponse: TransactionTargetResponse = {
-    chainId: `eip155:${fundsChainId}`,
-    method: "eth_sendTransaction",
-    params: {
-      abi: [],
-      to: relayTxData.to,
-      value: relayTxData.value,
-      data: relayTxData.data,
-    },
-  };
-
-  return Response.json(txResponse);
 };
